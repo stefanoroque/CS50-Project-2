2c614f03c65a40a5c969cceab7615c3c655c3392
stefanoroque <stefanoroque98@gmail.com>
2020-05-22 15:12:02 -0400

    created sql file to make db (not tested yet)

---
+++ create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username VARCHAR NOT NULL UNIQUE,
+    pword VARCHAR NOT NULL
+);
+
+CREATE TABLE books (
+    isbn INTEGER PRIMARY KEY,
+    title VARCHAR NOT NULL,
+    author VARCHAR NOT NULL,
+    pub_yr INTEGER,
+    review_count INTEGER,
+    avg_score INTEGER
+);
+
+CREATE TABLE reviews (
+    user_id INTEGER REFERENCES users,
+    book_isbn INTEGER REFERENCES books,
+    text_opinion VARCHAR NOT NULL,
+    rating INTEGER NOT NULL check(rating >= 0 and rating <= 5)
+);
