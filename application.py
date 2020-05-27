c017bbbab60941a692f6e868f30058b9a30c9455
stefanoroque <stefanoroque98@gmail.com>
2020-05-27 16:26:48 -0400

    can now send form data for registering an account, still need to post data to DB

--- application.py
+++ application.py
@@ -28,12 +28,35 @@ def index():
 @app.route("/register")
 def register():
     """Register an account."""
-
     return render_template("register.html")
 
-@app.route("/search", methods=["POST"])
-def search():
+@app.route("/accountCreation", methods=["POST"])
+def accountCreation():
     """search for a book."""
+
+    desired_username = request.form.get("desired_username")
+    pword = request.form.get("pword")
+
+    print("-----------")
+    print(desired_username)
+    print(pword)
+    print("-----------")
+
+    #TODO: now need to make sure username is not taken, and password is acceptable
+    #TODO: create another user in the database with this info
+
+    # try:
+    #     flight_id = int(request.form.get("flight_id"))
+    # except ValueError:
+    #     return render_template("error.html", message="Something went wrong. Try another username")
+
+    #   # Make sure the flight exists.
+    #   if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
+    #       return render_template("error.html", message="No such flight with that id.")
+    #   db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
+    #           {"name": name, "flight_id": flight_id})
+    #   db.commit()
+    return render_template("accountCreation.html")
 
 
 
