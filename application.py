357e35370b9449b8552e609d64173ccd957c9edf
stefanoroque <stefanoroque98@gmail.com>
2020-05-29 11:54:37 -0400

    now have loginAttempt, still need to pull user session when logged in so we can see user's reviews adn what not

--- application.py
+++ application.py
@@ -25,43 +25,39 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
+@app.route("/loginAttempt", methods=["POST"])
+def loginAttempt():
+    """Search for a book."""
+
+    username = request.form.get("username")
+    pword = request.form.get("pword")
+
+    if db.execute("SELECT * FROM users WHERE username = :username AND pword = :pword", {"username": username, "pword": pword}).rowcount == 0:  
+        return render_template("loginError.html", message="Incorrect username or password.")
+
+    return render_template("loginError.html", message="LOGGED IN")
+
 @app.route("/register")
 def register():
     """Register an account."""
     return render_template("register.html")
 
+
 @app.route("/accountCreation", methods=["POST"])
 def accountCreation():
-    """search for a book."""
+    """Account created and added to DB."""
 
     desired_username = request.form.get("desired_username")
     pword = request.form.get("pword")
 
-    print("-----------")
-    print(desired_username)
-    print(pword)
-    print("-----------")
-
-    #TODO: now need to make sure username is not taken, and password is acceptable
+    # Make sure username is not taken
     if db.execute("SELECT * FROM users WHERE username = :username", {"username": desired_username}).rowcount != 0:
         return render_template("registrationError.html", message="That username is already taken. Please choose a different username.")
     db.execute("INSERT INTO users (username, pword) VALUES (:username, :pword)",
             {"username": desired_username, "pword": pword})
+
+    # Create another user in the database with this info
     db.commit()
-
-    #TODO: create another user in the database with this info
-
-    # try:
-    #     flight_id = int(request.form.get("flight_id"))
-    # except ValueError:
-    #     return render_template("error.html", message="Something went wrong. Try another username")
-
-    #   # Make sure the flight exists.
-    #   if db.execute("SELECT * FROM flights WHERE id = :id", {"id": flight_id}).rowcount == 0:
-    #       return render_template("error.html", message="No such flight with that id.")
-    #   db.execute("INSERT INTO passengers (name, flight_id) VALUES (:name, :flight_id)",
-    #           {"name": name, "flight_id": flight_id})
-    #   db.commit()
     return render_template("accountCreation.html")
 
 
