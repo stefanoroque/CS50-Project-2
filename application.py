329d427f9ac15608f901a0d9ac4fac7807732f4c
stefanoroque <stefanoroque98@gmail.com>
2020-06-11 10:21:49 -0400

    commenting and code clean up

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 import os
 import requests
-
 from flask import Flask, session, render_template, request, abort, jsonify
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -38,10 +37,10 @@ def loginAttempt():
     if user_info.rowcount == 0:  
         return render_template("loginError.html", message="Incorrect username or password.")
 
-
     # Set session variable for the user's id
     session["user_id"] = user_info.fetchone().id
 
+    # Bring logged in user to search page
     return render_template("search.html")
 
 @app.route("/search", methods=["POST", "GET"])
@@ -51,27 +50,33 @@ def search():
     category = request.form.get("category")
     keyword = request.form.get("keyword")
 
+    # User left the keyword textbox blank
     if keyword == "":
         return render_template("search.html", no_keyword=True)
 
+    # Search by ISBN
     if category == "isbn":
         keyword = keyword.upper()
         books = db.execute("SELECT * FROM books WHERE isbn LIKE :isbn",
                             {"isbn": '%' + keyword + '%'}).fetchall()
 
+    # Search by book title
     elif category == "title":
         keyword = keyword.capitalize()
         books = db.execute("SELECT * FROM books WHERE title LIKE :title",
                             {"title": '%' + keyword + '%'}).fetchall()
 
+    # Search by author
     elif category == "author":
         keyword = keyword.capitalize()
         books = db.execute("SELECT * FROM books WHERE author LIKE :author",
                             {"author": '%' + keyword + '%'}).fetchall()
         
+    # User did not pick category to search by
     else:
         return render_template("search.html", missing_category=True)
     
+    # No book in the DB mached the search criteria
     if books == []:
         return render_template("search.html", no_book_found=True)
     else:
@@ -89,8 +94,10 @@ def book(book_isbn):
     # Update the session variable for book to the most recently viewed book
     session["book"] = book
 
+    # Find all existing reviews for the selected book
     reviews = db.execute("SELECT username, text_opinion, rating FROM (SELECT * FROM reviews WHERE book_isbn=:isbn) AS r INNER JOIN users ON r.user_id=users.id", {"isbn": book_isbn}).fetchall()
     
+    # Get review data from the Goodreads API
     goodreads_data = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "rILdgDWSEATeXeNiwPOzw", "isbns": book_isbn})
     if goodreads_data.status_code == 404:
         goodreads_data = False
@@ -104,10 +111,14 @@ def book(book_isbn):
 def reviewSubmission():
     """User leaves a review on a book."""
 
+    # Get the book we are reviewing from the user's session
     book = session["book"]
     book_isbn = book.isbn
+
+    # Find all existing reviews for the selected book
     reviews = db.execute("SELECT username, text_opinion, rating FROM (SELECT * FROM reviews WHERE book_isbn=:isbn) AS r INNER JOIN users ON r.user_id=users.id", {"isbn": book_isbn}).fetchall()
 
+    # Get review data from the Goodreads API
     goodreads_data = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "rILdgDWSEATeXeNiwPOzw", "isbns": book_isbn})
     if goodreads_data.status_code == 404:
         goodreads_data = False
@@ -116,6 +127,10 @@ def reviewSubmission():
 
     text_review = request.form.get("text_review")
 
+    # Make sure the text field is used
+    if text_review == "":
+        return render_template("book.html", book=book, reviews=reviews, goodreads_data=goodreads_data, empty_text_review=True)
+
     # Make sure a rating was chosen
     rating = request.form.get("rating")
     if rating == "Choose a rating from 1 to 5":
@@ -123,22 +138,19 @@ def reviewSubmission():
     else:
         rating = int(rating)
 
-    # Make sure the text field is used
-    if text_review == "":
-        return render_template("book.html", book=book, reviews=reviews, goodreads_data=goodreads_data, empty_text_review=True)
-
     # Make sure user has not already reviewed this book
     if db.execute("SELECT * FROM reviews WHERE user_id = :user_id AND book_isbn = :book_isbn", {"user_id": session["user_id"], "book_isbn": session["book"].isbn}).rowcount != 0:
         return render_template("book.html", book=book, reviews=reviews, goodreads_data=goodreads_data, already_reviewed=True)
 
+    # Add user review into DB
     db.execute("INSERT INTO reviews (user_id, book_isbn, text_opinion, rating) VALUES (:user_id, :book_isbn, :text_opinion, :rating)",
             {"user_id": session["user_id"], "book_isbn": session["book"].isbn, "text_opinion": text_review, "rating": rating})
 
-    #TODO: modify review count and average score for selected book
     # This is the first review of the book
     if session["book"].review_count is None:
         db.execute("UPDATE books SET review_count = 1, avg_score = :rating WHERE isbn = :book_isbn", {"rating": rating, "book_isbn": session["book"].isbn})
 
+    # Calculate new review count and average score for the book
     else:
         new_review_count = session["book"].review_count + 1
         new_avg_score = (session["book"].review_count * session["book"].avg_score + rating) / new_review_count
@@ -189,6 +201,7 @@ def api(book_isbn):
     if book is None:
         return abort(404, description="Book not found")
     
+    # Return JSON containing book info
     return jsonify(title=book.title,
                    author=book.author,
                    year=book.pub_yr,
